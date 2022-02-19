e727b1da624740a3260cac5eebcd4127201fc326
SHIN YUJIN <91702975+newjinny@users.noreply.github.com>
2022-02-19 15:36:46 +0900

    Time: 305 ms (16.67%), Space: 51.6 MB (13.44%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+   nums.sort();
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] == nums[i+1]){
+            return true;
+        }
+    }
+    return false;
+}
