17812d120836aed79fe277487754cd10ea35818d
SHIN YUJIN <91702975+newjinny@users.noreply.github.com>
2022-02-22 15:11:20 +0900

    Time: 72 ms (90.99%), Space: 41.9 MB (43.50%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.js
+++ 191-number-of-1-bits/191-number-of-1-bits.js
@@ -5,5 +5,10 @@
 var hammingWeight = function(n) {
     
 
-   return n.toString(2).match(/1/g)?.length ?? 0;
+let count = 0;
+  while (n !== 0){
+    n = n &(n-1);
+    count ++
+  }
+  return count;
 };     
