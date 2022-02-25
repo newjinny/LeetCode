2a769fb0cd4f2bae7cb1cef8fda8bc1bfcb35934
SHIN YUJIN <91702975+newjinny@users.noreply.github.com>
2022-02-25 14:45:40 +0900

    Time: 91 ms (44.61%), Space: 42.2 MB (14.03%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if (n <= 3) 
+      return n;
+  let step = [0,1,2]
+    for (let i = 3; i <= n; i++) {
+        step[i] = step[i - 1] + step[i - 2];
+    }
+    return step[n];
+};
