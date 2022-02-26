bd8387e17d6c607b99b9ca6499acd43c641986b8
SHIN YUJIN <91702975+newjinny@users.noreply.github.com>
2022-02-27 02:41:40 +0900

    Time: 74 ms (86.90%), Space: 44.6 MB (9.54%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.js
+++ 190-reverse-bits/190-reverse-bits.js
@@ -3,5 +3,6 @@
  * @return {number} - a positive integer
  */
 var reverseBits = function(n) {
-    return parseInt(n.toString(2).padStart(32, '0').split("").reverse().join(""), 2);
+    var newBit = n.toString(2).padStart(32,'0').split("").reverse().join("");
+    return parseInt(newBit, 2);
 };
