226a1efa8e1a015459eddcc53ef80677580d0481
SHIN YUJIN <91702975+newjinny@users.noreply.github.com>
2022-02-27 02:50:17 +0900

    Time: 151 ms (10.75%), Space: 43.5 MB (53.25%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.js
+++ 190-reverse-bits/190-reverse-bits.js
@@ -3,6 +3,8 @@
  * @return {number} - a positive integer
  */
 var reverseBits = function(n) {
-    var newBit = n.toString(2).padStart(32,'0').split("").reverse().join("");
-    return parseInt(newBit, 2);
+    //2진법 쪼개 뒤집어 합쳐
+    var bitReverse = n.toString(2).padStart(32,'0').split("").reverse().join("");
+    //padStart(32,'0')은 32길이에 부족하면 좌측부터 0으로 채워
+    return parseInt(bitReverse, 2);
 };
