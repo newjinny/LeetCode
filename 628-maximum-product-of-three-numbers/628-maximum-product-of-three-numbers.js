e4ee5ba7ce49d6964da0ce86133a3879972f3484
SHIN YUJIN <91702975+newjinny@users.noreply.github.com>
2022-03-11 23:23:24 +0900

    Time: 120 ms (70.92%), Space: 47.2 MB (13.65%) - LeetHub

---
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumProduct = function(nums) {
+      nums.sort( (a,b) => b-a)
+
+   return  Math.max(nums[0]*nums[1]*nums[2] , nums[0]*nums[nums.length-1] * nums[nums.length-2])
+    
+};
