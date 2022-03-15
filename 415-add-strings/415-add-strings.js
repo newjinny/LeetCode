49f6d96ca3aabfb201dda5502bfd750c23b3bbf0
YUJIN SHIN <syj9437@gmail.com>
2022-03-15 23:43:25 +0900

    Time: 64 ms (98.07%), Space: 42.7 MB (72.43%) - LeetHub

--- 415-add-strings/415-add-strings.js
+++ 415-add-strings/415-add-strings.js
@@ -4,6 +4,6 @@
  * @return {string}
  */
 var addStrings = function(num1, num2) {
-       let result = (BigInt(num1)+BigInt(num2)).toString();
-   return result;
+      return (BigInt(num1)+BigInt(num2)).toString();
+ 
 };
