e298080bd830b2282cd7f273164406857f6d5c0d
YUJIN SHIN <syj9437@gmail.com>
2022-03-16 14:42:58 +0900

    Time: 93 ms (61.95%), Space: 44.9 MB (30.58%) - LeetHub

--- 415-add-strings/415-add-strings.js
+++ 415-add-strings/415-add-strings.js
@@ -4,6 +4,21 @@
  * @return {string}
  */
 var addStrings = function(num1, num2) {
-      return (BigInt(num1)+BigInt(num2)).toString();
+      // return (BigInt(num1)+BigInt(num2)).toString();
+      let carry = 0;
+    let result = [];
+    
+    for(let i = num1.length-1, j = num2.length-1 ; i>=0 || j>=0; i--, j--) { 
+        let sum = getNum(num1[i]) + getNum(num2[j]) + carry;
+        carry = sum > 9 ? 1: 0;
+        result.push(sum % 10);
+    }
+    
+    if(carry > 0) result.push(carry);
+    return result.reverse().join('');
+};
+
+function getNum(num) {
+    return num ? (num-0) : 0
  
 };
