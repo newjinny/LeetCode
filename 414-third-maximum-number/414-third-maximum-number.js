059cccb0a13b5a820dfc33610c24aac332d02ff8
YUJIN SHIN <syj9437@gmail.com>
2022-03-25 00:56:10 +0900

    Time: 106 ms (33.45%), Space: 45 MB (21.22%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.js
+++ 414-third-maximum-number/414-third-maximum-number.js
@@ -4,6 +4,6 @@
  */
 var thirdMax = function(nums) {
     let set = Array.from(new Set(nums));
-    
+    // console.log(set.length)
     return set.length < 3? set.sort((a,b) => b-a)[0]:set.sort((a,b) => b-a)[2]
 };
