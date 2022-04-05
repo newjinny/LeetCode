125f2b68977dfd3a99ba5daea34ab5c6d67158ef
YUJIN SHIN <syj9437@gmail.com>
2022-04-05 14:32:27 +0900

    Time: 100 ms (16.17%), Space: 41.7 MB (89.17%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.js
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.js
@@ -3,13 +3,12 @@
  * @return {number}
  */
 var average = function(salary) {
-    var max = Math.max(...salary);
-    var min = Math.min(...salary);
-    
-  let sum = salary.reduce((a,b) => a + b)
-    
+    let max = Math.max(...salary);
+    let min = Math.min(...salary);
+    let sum = salary.reduce((a,b) => a + b) 
     sum -= max
     sum -= min
+    
     
     return sum / (salary.length - 2)
 };
