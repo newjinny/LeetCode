3c4ecc0e9b6d0add2202cd07e47daa77560134dd
YUJIN SHIN <syj9437@gmail.com>
2022-04-09 00:27:38 +0900

    Time: 124 ms (19.31%), Space: 44.6 MB (64.64%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.js
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.js
@@ -4,8 +4,8 @@
  */
 var divideArray = function(nums) {
     let arr = nums.sort((a,b) => b-a)
-    for (i =1; i<arr.length; i+=2){
-        if(arr[i] !== arr[i-1]) 
+    for (i =0; i<arr.length; i+=2){
+        if(arr[i] !== arr[i+1]) 
             return false
     }
    return true
