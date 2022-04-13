8220b177d25c91cfece126c37f6a3cdd7b97a0d7
YUJIN SHIN <syj9437@gmail.com>
2022-04-14 00:15:41 +0900

    Time: 77 ms (71.54%), Space: 43.8 MB (69.03%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.js
+++ 832-flipping-an-image/832-flipping-an-image.js
@@ -9,9 +9,3 @@ var flipAndInvertImage = function(image) {
     }
         return result
 };
-//         const map=[]
-// 	for(let i=0; i< image.length; i++){
-// 		map.push(image[i].map((a)=> a===1 ? 0:1).reverse())
-// 	}
-//    return map
-// };
