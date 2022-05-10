a21314c554f7087e0ed327fa3263012a929903bb
YUJIN SHIN <syj9437@gmail.com>
2022-05-10 23:28:03 +0900

    Time: 215 ms (7.10%), Space: 54.2 MB (14.14%) - LeetHub

--- 338-counting-bits/338-counting-bits.js
+++ 338-counting-bits/338-counting-bits.js
@@ -6,6 +6,7 @@ var countBits = function(n) {
     let arr = [];
     for(let i=0; i <= n; i++){
         arr.push(i.toString(2).split("1").length-1);
+        // arr.push(i.toString(2).split('0').join('').length);
 
     
     
