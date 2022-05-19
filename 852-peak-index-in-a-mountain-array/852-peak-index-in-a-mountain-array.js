5d3ca2dd95fa21c2f5be67c2c76e4aac43a3e99b
YUJIN SHIN <syj9437@gmail.com>
2022-05-19 21:46:23 +0900

    Time: 74 ms (60.03%), Space: 43.1 MB (8.74%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.js
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.js
@@ -3,11 +3,14 @@
  * @return {number}
  */
 var peakIndexInMountainArray = function(arr) {
-    for(let i=0; i<arr.length; i++){
-        if(arr[i]>arr[i+1]){
-            return i
-        }
-    }
+    const max = Math.max(...arr)
+    console.log(Math.max(...arr))
+    return arr.indexOf(max)
+    // for(let i=0; i<arr.length; i++){
+    //     if(arr[i]>arr[i+1]){
+    //         return i
+    //     }
+    // }
     
 };
 
