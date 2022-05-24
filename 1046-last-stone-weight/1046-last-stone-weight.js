21b3854c194bd09cf326ed1912f4397023d6a04e
YUJIN SHIN <syj9437@gmail.com>
2022-05-24 23:48:48 +0900

    Time: 65 ms (83.68%), Space: 44 MB (28.41%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function(stones) {
+       while(stones.length>1){
+        stones.sort((a,b)=>b-a); 
+        stones[1]=stones[0]-stones[1]; 
+        stones.shift();
+    }
+    return stones[0]
+//        while (stones.length > 1){
+
+//         stones.sort((a,b) => a-b);
+
+//         let x = stones.pop();
+//         let y = stones.pop();
+
+//         if (x!=y){
+//             stones.push(x-y)
+//         }
+
+//     }
+
+//     return stones[0] || 0;
+};
