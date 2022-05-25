86ad9104d6eb385d20117d4e609f081588634cc3
YUJIN SHIN <syj9437@gmail.com>
2022-05-26 00:02:59 +0900

    Time: 129 ms (71.03%), Space: 57 MB (87.50%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.js
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.js
@@ -3,5 +3,7 @@
  * @return {number}
  */
 var findCenter = function(edges) {
-    return edges[0][0] === edges[1][0] || edges[0][0] === edges[1][1] ? edges[0][0] : edges[0][1];
+    // console.log(edges[0][0])
+    return edges.every( res => res.includes( edges[0][0] )) ? edges[0][0] : edges[0][1]
+    // edges[0][0] === edges[1][0] || edges[0][0] === edges[1][1] ? edges[0][0] : edges[0][1];
 };
