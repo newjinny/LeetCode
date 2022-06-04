6b9c1053d750487d84ea44ad972aafe519118034
YUJIN SHIN <syj9437@gmail.com>
2022-06-04 10:08:13 +0900

    Time: 97 ms (48.46%), Space: 49.3 MB (14.62%) - LeetHub

---
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var findSpecialInteger = function(arr) {
+   const obj = {}
+   arr.map(num => {
+       obj[num] = (obj[num] || 0 ) + 1
+   })
+    
+    for (let num in obj){
+        if( obj[num] > arr.length / 4)
+            return Number(num)
+    }
+    
+};
