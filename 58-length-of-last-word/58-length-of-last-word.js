f0cbfaf37d0d4570461070ca12f87d7d49507b77
YUJIN SHIN <syj9437@gmail.com>
2022-07-07 00:07:03 +0900

    Time: 104 ms (19.01%), Space: 41.9 MB (74.87%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.js
+++ 58-length-of-last-word/58-length-of-last-word.js
@@ -3,5 +3,9 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-   return s.trim().split(' ').pop().length
+    let arr = s.split(' ')
+    let result = arr.filter(i => i.length !== 0)
+    return result[result.length-1].length
+   // return s.trim().split(' ').pop().length
+
 };
