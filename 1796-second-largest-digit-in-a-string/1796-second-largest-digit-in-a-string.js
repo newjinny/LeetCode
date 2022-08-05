c6f9e29660add383eedd5e5dfa230464d0abd45f
YUJIN SHIN <syj9437@gmail.com>
2022-08-06 00:10:21 +0900

    Time: 126 ms (22.37%), Space: 44.4 MB (41.45%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.js
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.js
@@ -4,12 +4,14 @@
  */
 var secondHighest = function(s) {
     
-    const regex = /[^0-9]/g;
-    const number = s.replace(regex, '').split('').sort((a, b) => b - a);
-    const arr =[...( new Set(number))];
+    // const regex = /[^0-9]/g;
+    // const number = s.replace(/[^0-9]/g, '').split('').sort((a, b) => b - a);
+    // const arr =[...( new Set(number))].
     
 
 
-   return  arr.length == 1 || arr.length == 0 ? -1 : arr[1];
+   // return  arr.length == 1 || arr.length == 0 ? -1 : arr[1];
+    
+    return [...(new Set(s.replace(/[^0-9]/g, '').split('').sort((a, b) => b - a)))].length === 0 || [...(new Set(s.replace(/[^0-9]/g, '').split('').sort((a, b) => b - a)))].length === 1? -1 :[...(new Set(s.replace(/[^0-9]/g, '').split('').sort((a, b) => b - a)))][1]
 
 };
