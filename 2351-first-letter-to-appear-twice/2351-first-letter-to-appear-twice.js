86c34638a66b02dc3db8729851be118a3ba84da7
YUJIN SHIN <syj9437@gmail.com>
2022-08-19 16:27:53 +0900

    Time: 111 ms (11.76%), Space: 41.8 MB (72.60%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.js
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.js
@@ -3,13 +3,22 @@
  * @return {character}
  */
 var repeatedCharacter = function(s) {
-    const newSet = {}
-    
-    for(i = 0; i < s.length; i++){
-        if(!newSet[s[i]]){
-            newSet[s[i]] = 1
-        } else{
-            return s[i]
-        }
+    const set = new Set()
+    for(let char of s){
+        if(set.has(char))
+            return char
+            set.add(char)
+        
     }
 };
+  
+//     const newSet = {}
+    
+//     for(i = 0; i < s.length; i++){
+//         if(!newSet[s[i]]){
+//             newSet[s[i]] = 1
+//         } else{
+//             return s[i]
+//         }
+//     }
+// };
