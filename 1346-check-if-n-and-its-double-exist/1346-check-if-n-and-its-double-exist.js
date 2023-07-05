0d6014bb677a12e98258dabcce591c51c4ae776f
YUJIN SHIN <syj9437@gmail.com>
2023-07-05 23:20:50 +0900

    Time: 64 ms (47.57%), Space: 43.4 MB (37.35%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
@@ -3,9 +3,9 @@
  * @return {boolean}
  */
 var checkIfExist = function(arr) {
-    for(let i=0;i<arr.length;i++){
-        for(let j=0;j<arr.length;j++){
-            if ( i!==j && arr[i]==2*arr[j]){
+    for(let i=0;i<arr.length-1;i++){
+        for(let j=i+1;j<arr.length;j++){
+            if (arr[i]===2*arr[j] || arr[i]===0.5*arr[j]){
                 return true;
             }
         }
